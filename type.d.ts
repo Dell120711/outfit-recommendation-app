04ff531b6a57884f54729a9b12b03ffceec3ab06
YONG CHEN DAI <tacoco907@gmail.com>
2025-05-07 02:10:46 +0800

    .

---
+++ type.d.ts
@@ -0,0 +1,47 @@
+import { Favorite, Item, Param, Profile, Recommendation, Result, Suggestion, Upload } from "@prisma/client";
+
+export type ItemTable = Item;
+export type ParamTable = Param;
+export type RecommendationTable = Recommendation;
+export type SuggestionTable = Suggestion;
+export type UploadTable = Upload;
+export type ResultTable = Result;
+export type ProfileTable = Profile;
+export type FavoriteTable = Favorite;
+
+export type SimplifiedItemTable = Omit<Item, 'embedding'>;
+export type Series = { items: SimplifiedItemTable[], isFavorite: boolean };
+
+export interface Recommendation {
+  clothingType: ClothingType;
+  gender: Gender;
+  model: string;
+  imageUrl: string;
+  styles: {
+    [styleName: string]: { suggestion_id: number, series: Series[], description: string };
+  };
+}
+
+export interface SearchResult {
+  series: Series[];
+  totalPages: number;
+}
+
+export type RecommendationPreview = Recommendation & {
+  upload: UploadTable;
+};
+
+type ValidatedRecommendation = {
+  styleName: string;
+  description: string;
+  labelString: string;
+};
+export interface UnstoredResult {
+  distance: number;
+  item_id: string;
+  suggestion_id: number;
+}
+
+export type ClothingType = Enums<"clothing_type">;
+export type BodyType = Enums<"body_type">;
+export type Gender = Enums<"gender">;
