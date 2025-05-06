bf74a96d37498b71cbadde84ef53dc4e29989f8a
YONG CHEN DAI <tacoco907@gmail.com>
2025-05-07 02:12:14 +0800

    .

---
+++ next.config.js
@@ -0,0 +1,135 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dmbkhireuarjpvecjmds.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'jmgowbnhsejplwjfhpnv.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.discordapp.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'eapzlwxcyrinipmcdoir.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.uniqlo.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.uniqlo.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.hicloud.net.tw',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.50-shop.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lp2.hm.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.gu-global.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.lativ.com.tw',
+      },
+      {
+        protocol: 'https',
+        hostname: 's.lativ.com.tw',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.meierq.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pics.meierq.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.net-fashion.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'net.wimg.tw',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.obdesign.com.tw',
+      },
+      {
+        protocol: 'https',
+        hostname: 'diz36nn4q02zr.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.pazzo.com.tw',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pics.pzcdn.tw',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.zara.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.zara.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.edwin.com.tw',
+      },
+      {
+        protocol: 'https',
+        hostname: 'diz36nn4q02zr.cloudfront.net'
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.alasha.com.tw'
+      }, 
+      {
+        protocol: 'https',
+        hostname: 'www.roots.com.tw'
+      }, 
+      {
+        protocol: 'https',
+        hostname: 'cdn-static.roots.com.tw'
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent-tpe1-1.xx.fbcdn.net',
+      }
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "4mb", // Set desired value here
+    },
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
