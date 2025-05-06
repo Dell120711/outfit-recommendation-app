52e1386f5499f966197cc27e89f6c5cc98ead5fa
YONG CHEN DAI <tacoco907@gmail.com>
2025-05-07 02:51:33 +0800

    Add files via upload

---
+++ app/customization-fields.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { useFormContext } from "react-hook-form";
+
+
+const CustomizationFields = () => {
+  const {
+    register,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useFormContext();
+
+  const clothingType = watch("clothingType");
+  const gender = watch("gender");
+
+  return (
+    <Card className='w-128 max-w-[92vw] bg-white'>
+      <CardHeader>
+        <CardTitle>客製化參數</CardTitle>
+        <CardDescription>
+          讓模型知道更多資訊以提供您更精確的穿搭建議！
+        </CardDescription>
+      </CardHeader>
+      <CardContent className='grid gap-6'>
+        <div className='grid gap-2'>
+          <Label htmlFor='gender'>我想搜尋的服飾性別為</Label>
+          <div className='flex flex-wrap gap-2'>
+            <Label
+              htmlFor='gender-male'
+              className={cn(
+                'flex items-center gap-2 rounded-md border px-3 py-2 cursor-pointer',
+                gender === "male" && "bg-muted"
+              )}
+            >
+              <input
+                id='gender-male'
+                type='radio'
+                value='male'
+                {...register("gender")}
+                onChange={() => setValue("gender", "male")}
+              />
+              男性
+            </Label>
+            <Label
+              htmlFor='gender-female'
+              className={cn(
+                'flex items-center gap-2 rounded-md border px-3 py-2 cursor-pointexr',
+                gender === "female" && "bg-muted"
+              )}
+            >
+              <input
+                id='gender-female'
+                type='radio'
+                value='female'
+                {...register("gender")}
+                onChange={() => setValue("gender", "female")}
+              />
+              女性
+            </Label>
+            <Label
+              htmlFor='gender-neutral'
+              className={cn(
+                'flex items-center gap-2 rounded-md border px-3 py-2 cursor-pointer',
+                gender === "neutral" && "bg-muted"
+              )}
+            >
+              <input
+                id='gender-neutral'
+                type='radio'
+                value='neutral'
+                {...register("gender")}
+                onChange={() => {
+                  console.log('neutral')
+                  setValue("gender", "neutral")
+                }}
+              />
+              無限制
+            </Label>
+          </div>
+          {errors.gender && (
+            <span className='text-red-600'>
+              {errors.gender.message?.toString()}
+            </span>
+          )}
+        </div>
+        <div className='grid gap-2'>
+          <Label htmlFor='clothing-type'>我上傳的服飾是</Label>
+          <div className='flex flex-wrap gap-2'>
+            <Label
+              htmlFor='clothing-type-top'
+              className={`flex items-center gap-2 rounded-md border px-3 py-2 cursor-pointer ${clothingType === "top" ? "bg-muted" : ""
+                }`}
+            >
+              <input
+                id='clothing-type-top'
+                type='radio'
+                value='top'
+                {...register("clothingType")}
+                onChange={() => setValue("clothingType", "top")}
+              />
+              上衣
+            </Label>
+            <Label
+              htmlFor='clothing-type-bottom'
+              className={`flex items-center gap-2 rounded-md border px-3 py-2 cursor-pointer ${clothingType === "bottom" ? "bg-muted" : ""
+                }`}
+            >
+              <input
+                id='clothing-type-bottom'
+                type='radio'
+                value='bottom'
+                {...register("clothingType")}
+                onChange={() => setValue("clothingType", "bottom")}
+              />
+              下身
+            </Label>
+          </div>
+          {errors.clothingType && (
+            <span className='text-red-600'>
+              {errors.clothingType.message?.toString()}
+            </span>
+          )}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CustomizationFields;
---
+++ app/image-uploader.tsx
@@ -0,0 +1,88 @@
+"use client";
+import imageCompression from "browser-image-compression";
+import { Upload } from "lucide-react";
+import Image from "next/image";
+import { useRef, useState } from "react";
+import { useFormContext } from "react-hook-form";
+
+const ImageUploader = ({
+  onImageUpload,
+}: {
+  onImageUpload: () => void;
+}) => {
+  const {
+    register,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useFormContext();
+
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleFileClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    onImageUpload();
+    const files = e.target.files;
+    const options = {
+      maxSizeMB: .1,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true,
+      fileType: "image/jpeg"
+    }
+    if (files && files[0]) {
+      const compressedFile = await imageCompression(files[0], options);
+      console.log('uploaded file(s):', files);
+      console.log('compressed file(s):', compressedFile);
+      setValue("uploadedImage", compressedFile);
+      setPreview(URL.createObjectURL(compressedFile));
+      console.log(getValues());
+    }
+  };
+
+  return (
+    <div className='flex flex-col items-center justify-center gap-2 '>
+      <div
+        onClick={handleFileClick}
+        className='cursor-pointer rounded-sm w-80 h-80 border-2 border-dashed border-gray-400/80 flex-col flex gap-4 items-center justify-center relative'
+      >
+        {preview ? (
+          <Image
+            src={preview}
+            alt='Uploaded Image'
+            fill
+            className='object-cover'
+          />
+        ) : (
+          <Upload className='w-10 h-10 text-gray-400' />
+        )}
+        <div className='mb-2 text-sm text-gray-500'>
+          <span className='font-semibold text-indigo-400'>點擊上傳</span>{" "}
+          或拖放圖片
+        </div>
+        <input
+          {...register("uploadedImage")}
+          ref={fileInputRef}
+          id='uploadedImage'
+          className='hidden'
+          type='file'
+          accept='image/*'
+          onChange={handleFileChange}
+        />
+      </div>
+      {errors.uploadedImage && (
+        <span className='text-red-600'>
+          {errors.uploadedImage.message?.toString()}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default ImageUploader;
